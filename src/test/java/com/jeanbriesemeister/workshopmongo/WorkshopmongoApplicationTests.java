b44dff0716c6b427729a230fe31965879fadf8eb
Jean Briesemeister <jeanbr86@gmail.com>
2020-03-26 11:33:17 -0300

    Project created

---
+++ src/test/java/com/jeanbriesemeister/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeanbriesemeister.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
