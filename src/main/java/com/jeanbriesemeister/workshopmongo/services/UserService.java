0b81fe34ba58582798d62bb9c1846ca8ad959892
Jean Briesemeister <jeanbr86@gmail.com>
2020-03-26 22:24:13 -0300

    Added object not found exception

--- src/main/java/com/jeanbriesemeister/workshopmongo/services/UserService.java
+++ src/main/java/com/jeanbriesemeister/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.jeanbriesemeister.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jeanbriesemeister.workshopmongo.domain.User;
 import com.jeanbriesemeister.workshopmongo.repository.UserRepository;
+import com.jeanbriesemeister.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,11 @@ public class UserService {
 
 	public List<User> findAll() {
 		return userRepository.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+
 	}
 }
