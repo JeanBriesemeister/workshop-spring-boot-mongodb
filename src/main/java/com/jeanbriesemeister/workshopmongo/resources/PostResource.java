45d00ae823b1ac5a641b8ad502ef8f4eeeb57a0e
Jean Briesemeister <jeanbr86@gmail.com>
2020-04-09 21:56:17 -0300

    Added query by post title

--- src/main/java/com/jeanbriesemeister/workshopmongo/resources/PostResource.java
+++ src/main/java/com/jeanbriesemeister/workshopmongo/resources/PostResource.java
@@ -1,10 +1,15 @@
 package com.jeanbriesemeister.workshopmongo.resources;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jeanbriesemeister.workshopmongo.domain.Post;
@@ -20,6 +25,17 @@ public class PostResource {
 	@RequestMapping(method = RequestMethod.GET, value = "/{id}")
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		return ResponseEntity.ok().body(postService.findById(id));
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		try {
+			text = URLDecoder.decode(text, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			text = "";
+		}
+
+		return ResponseEntity.ok().body(postService.findByTitle(text));
 	}
 
 }
