84cce82def7002aea792ea84f48043215d8277dd
Jean Briesemeister <jeanbr86@gmail.com>
2020-04-09 22:24:14 -0300

    Added search using date and text

--- src/main/java/com/jeanbriesemeister/workshopmongo/resources/PostResource.java
+++ src/main/java/com/jeanbriesemeister/workshopmongo/resources/PostResource.java
@@ -2,7 +2,11 @@ package com.jeanbriesemeister.workshopmongo.resources;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -36,6 +40,36 @@ public class PostResource {
 		}
 
 		return ResponseEntity.ok().body(postService.findByTitle(text));
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/fullsearch")
+	public ResponseEntity<List<Post>> fullSearch(@RequestParam(value = "text", defaultValue = "") String text,
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+		try {
+			text = URLDecoder.decode(text, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			text = "";
+		}
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+		Date min;
+		Date max;
+		try {
+			min = sdf.parse(minDate);
+		} catch (ParseException e) {
+			min = new Date(0L);
+		}
+
+		try {
+			max = sdf.parse(maxDate);
+		} catch (ParseException e) {
+			max = new Date();
+		}
+
+		return ResponseEntity.ok().body(postService.fullSearch(text, min, max));
 	}
 
 }
